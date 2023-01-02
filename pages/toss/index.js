02a32c431cb22ecc9ba08e14c3da1313fc63c442
EunhaLim <dmsgk1559@gmail.com>
2023-01-02 18:02:04 +0900

    easyPay - toss example

--- pages/toss/index.js
+++ pages/toss/index.js
@@ -9,14 +9,34 @@ const Toss = () => {
 
     loadTossPayments(clientKey)
       .then((tossPayments) => {
-        console.log('결제중???');
+        console.log('신용, 체크 카드 결제');
         tossPayments.requestPayment('카드', {
           amount: 38000,
-          orderId: 'vegaamond---',
+          orderId: 'daemyunslayer9',
           orderName: '토스 마스크 외 1건',
           customerName: '아갈인파이터',
           successUrl: `${API_URL}api/toss/success`,
           failUrl: `${API_URL}api/toss/fail`,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const easyTossPay = () => {
+    loadTossPayments(clientKey)
+      .then((tossPayments) => {
+        console.log('간편결제 - 토스페이');
+        tossPayments.requestPayment('카드', {
+          amount: 20000,
+          orderId: 'easyPay-TossPay10',
+          orderName: '토스 티셔츠 외 2건',
+          customerName: '배가아몬드',
+          successUrl: `${API_URL}api/toss/success`,
+          failUrl: `${API_URL}api/toss/fail`,
+          flowMode: 'DIRECT',
+          easyPay: '토스페이',
         });
       })
       .catch((err) => {
@@ -30,6 +50,7 @@ const Toss = () => {
       <p>Test Client Key: {clientKey}</p>
       <p>Test Secret Key: {secretKey}</p>
       <button onClick={pay}>결제하기</button>
+      <button onClick={easyTossPay}>간편결제 - 토스페이</button>
     </>
   );
 };
